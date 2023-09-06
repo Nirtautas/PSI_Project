e33fccf8cd8da75d0bb515443ef0b5345cf273b9
Nirtautas <nirtautas.sadauskas@gmail.com>
2023-09-06 12:55:13 +0300

    Fixes as requested Back button now returns to course list as intended. Added placeholder controller for the environment inside the course.

---
+++ TeamWebApplication/Controllers/CourseEnvironmentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamWebApplication.Controllers
+{
+    public class CourseEnvironmentController : Controller
+    {
+        /*
+         * This controller is a placeholder
+         * It should control the content displayed in an exact course
+         * It should do so by fetching correct data from database (Of which code does not yet exist)
+         */
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
