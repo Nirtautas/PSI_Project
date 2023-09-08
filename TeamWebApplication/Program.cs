74238baf87831a393b5156666f020bf3f8d36840
Nirte <nirtautas.sadauskas@gmail.com>
2023-09-08 19:06:39 +0300

    Fixed singleton pattern

--- TeamWebApplication/Program.cs
+++ TeamWebApplication/Program.cs
@@ -27,7 +27,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-LocalDataContainer LocalDatabase = new LocalDataContainer();
+LocalDataContainer LocalDatabase = LocalDataContainer.Instance;
 LocalDatabase.fetchLocalData();
 LocalDatabase.printLocalData();
 
