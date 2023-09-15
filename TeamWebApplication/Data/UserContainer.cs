ddd392586a0863b873e7d2e68af4703c7ea5991c
Brigita Kasperavičiūtė <brigita.kasperaviciute@mif.stud.vu.lt>
2023-09-15 14:18:18 +0300

    Fixed some bugs

--- TeamWebApplication/Data/UserContainer.cs
+++ TeamWebApplication/Data/UserContainer.cs
@@ -15,73 +15,73 @@ namespace TeamWebApplication.Data
                 instance = new UserContainer();
             return instance;
         }
-    }
 
-    public void FetchUsers()
-    {
-        string? readString;
-        string[]? splitString;
 
-        using (StreamReader? reader = new StreamReader("./TextData/UserData.txt"))
+        public void FetchUsers()
         {
-            if ((readString = reader.ReadLine()) != null)
-                userIdCounter = Int32.Parse(readString);
+            string? readString;
+            string[]? splitString;
 
-            while ((readString = reader.ReadLine()) != null)
+            using (StreamReader? reader = new StreamReader("./TextData/UserData.txt"))
             {
-                splitString = readString.Split(';');
-                switch (splitString[6])
+                if ((readString = reader.ReadLine()) != null)
+                    userIdCounter = Int32.Parse(readString);
+
+                while ((readString = reader.ReadLine()) != null)
                 {
-                    case "Student":
-                        Student student = new Student(
-                            Int32.Parse(splitString[0]),                                                     //UserId
-                            splitString[1],                                                                  //Name
-                            splitString[2],                                                                  //Surname
-                            DateTime.ParseExact(splitString[3], "yyyy-MM-dd", CultureInfo.InvariantCulture), //Birth Date
-                            splitString[4],                                                                  //Email
-                            splitString[5],                                                                  //Password
-                            (Role)Enum.Parse(typeof(Role), splitString[6]),                                  //Role
-                            (Faculty)Enum.Parse(typeof(Faculty), splitString[7]),                            //Faculty
-                            (Specialization)Enum.Parse(typeof(Specialization), splitString[8]),              //Specialization
-                            (AcademicDegree)Enum.Parse(typeof(AcademicDegree), splitString[9]),              //Academic Degree
-                            Int32.Parse(splitString[10])                                                     //YearIn
-                        );
-                        _userList.Add(student);
-                        break;
-                    case "Lecturer":
-                        Lecturer lecturer = new Lecturer(
-                            Int32.Parse(splitString[0]),                                                     //UserId
-                            splitString[1],                                                                  //Name
-                            splitString[2],                                                                  //Surname
-                            DateTime.ParseExact(splitString[3], "yyyy-MM-dd", CultureInfo.InvariantCulture), //Birth Date
-                            splitString[4],                                                                  //Email
-                            splitString[5],                                                                  //Password
-                            (Role)Enum.Parse(typeof(Role), splitString[6]),                                  //Role
-                            (Faculty)Enum.Parse(typeof(Faculty), splitString[7]),                            //Faculty
-                            (Specialization)Enum.Parse(typeof(Specialization), splitString[8]),              //Specialization
-                            (Title)Enum.Parse(typeof(Title), splitString[9])                                 //Title
-                        );
-                        _userList.Add(lecturer);
-                        break;
+                    splitString = readString.Split(';');
+                    switch (splitString[6])
+                    {
+                        case "Student":
+                            Student student = new Student(
+                                Int32.Parse(splitString[0]),                                                     //UserId
+                                splitString[1],                                                                  //Name
+                                splitString[2],                                                                  //Surname
+                                DateTime.ParseExact(splitString[3], "yyyy-MM-dd", CultureInfo.InvariantCulture), //Birth Date
+                                splitString[4],                                                                  //Email
+                                splitString[5],                                                                  //Password
+                                (Role)Enum.Parse(typeof(Role), splitString[6]),                                  //Role
+                                (Faculty)Enum.Parse(typeof(Faculty), splitString[7]),                            //Faculty
+                                (Specialization)Enum.Parse(typeof(Specialization), splitString[8]),              //Specialization
+                                (AcademicDegree)Enum.Parse(typeof(AcademicDegree), splitString[9]),              //Academic Degree
+                                Int32.Parse(splitString[10])                                                     //YearIn
+                            );
+                            _userList.Add(student);
+                            break;
+                        case "Lecturer":
+                            Lecturer lecturer = new Lecturer(
+                                Int32.Parse(splitString[0]),                                                     //UserId
+                                splitString[1],                                                                  //Name
+                                splitString[2],                                                                  //Surname
+                                DateTime.ParseExact(splitString[3], "yyyy-MM-dd", CultureInfo.InvariantCulture), //Birth Date
+                                splitString[4],                                                                  //Email
+                                splitString[5],                                                                  //Password
+                                (Role)Enum.Parse(typeof(Role), splitString[6]),                                  //Role
+                                (Faculty)Enum.Parse(typeof(Faculty), splitString[7]),                            //Faculty
+                                (Specialization)Enum.Parse(typeof(Specialization), splitString[8]),              //Specialization
+                                (Title)Enum.Parse(typeof(Title), splitString[9])                                 //Title
+                            );
+                            _userList.Add(lecturer);
+                            break;
+                    }
                 }
             }
         }
-    }
 
-    public void WriteUsers()
-    {
-        using (StreamWriter? writer = new StreamWriter("./TextData/UserData.txt"))
+        public void WriteUsers()
         {
-            writer.WriteLine(userIdCounter);
+            using (StreamWriter? writer = new StreamWriter("./TextData/UserData.txt"))
+            {
+                writer.WriteLine(userIdCounter);
+                foreach (var user in _userList)
+                    writer.WriteLine(user.ToString());
+            }
+        }
+
+        public void PrintUserList()
+        {
             foreach (var user in _userList)
-                writer.WriteLine(user.ToString());
+                System.Diagnostics.Debug.WriteLine(user.ToString());
         }
     }
-
-    public void PrintUserList()
-    {
-        foreach (var user in _userList)
-            System.Diagnostics.Debug.WriteLine(user.ToString());
-    }
-}
 }
