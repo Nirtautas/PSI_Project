be10ec29a98a4812bcd196afa0c3a28cbc865b5d
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2023-09-24 19:47:53 +0300

    Changed LoginDetails.cs to a record

--- TeamWebApplication/Controllers/LogInController.cs
+++ TeamWebApplication/Controllers/LogInController.cs
@@ -31,7 +31,7 @@ namespace TeamWebApplication.Controllers
         {
             var user = _userContainer.userList.SingleOrDefault(user => user.Password == login.Password && user.UserId == login.UserId);
 			if (user == null)
-                return RedirectToAction("Index", "Login"); ;//user not found}
+                return RedirectToAction("Index", "Login");//user not found}
             _userContainer.loggedInUserId = user.UserId;
 			return RedirectToAction("Index", "Course");//Index - action, Course - controller, user - object (user that signed in)
         }
